38ac266c02d5bb28c9c6018cc708bd290cbec7c6
Michel de Bree <michel@micheldebree.nl>
2024-04-02 08:57:02 +0200

    csdbCal: Better formatting of location

--- csdbCal/calendar.go
+++ csdbCal/calendar.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
-	"github.com/arran4/golang-ical"
+	ics "github.com/arran4/golang-ical"
 )
+
+func commaSeparate(text ...string) string {
+	result := ""
+
+	for _, txt := range text {
+		trimmedText := strings.TrimSpace(txt)
+		if len(trimmedText) > 0 {
+			if len(result) > 0 {
+				result += ", " + trimmedText
+			} else {
+				result += trimmedText
+			}
+		}
+	}
+	return result
+}
 
 func createCalender(events []EventElement) string {
 	cal := ics.NewCalendar()
@@ -15,7 +32,7 @@ func createCalender(events []EventElement) string {
 		calEvent.SetSummary(event.Name)
 		calEvent.SetDescription(event.EventType)
 		calEvent.SetURL(event.Website)
-		calEvent.SetLocation(fmt.Sprintf("%s, %s, %s", event.Address, event.City, event.Country))
+		calEvent.SetLocation(commaSeparate(event.Address, event.City, event.Country))
 
 		startDate := time.Date(event.StartYear, time.Month(event.StartMonth), event.StartDay, 0, 0, 0, 0, time.UTC)
 		endDate := time.Date(event.EndYear, time.Month(event.EndMonth), event.EndDay, 0, 0, 0, 0, time.UTC)
